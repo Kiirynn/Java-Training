d5298c62548abbf2443b576b4ff825936e06c758
Kiirynn <kieran.poropat@gmail.com>
2022-11-26 13:46:26 -0500

    still having troubles getting it to fire

--- java.js
+++ java.js
@@ -361,8 +361,9 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.links');
+    const links = document.querySelectorAll('.links li');
      //toggle nav
-    burger.addEventListener('click', function (){
+    burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
        
        //animate links
@@ -371,7 +372,7 @@ const navSlide = () => {
              link.style.animation = '';
              }
              else {
-                 link.style.animation =  `navLinkFade 0.5s eae forwards ${index / 7 + 0.3}s`;
+                 link.style.animation =  `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
              }
          });
 
