c832923db742876a336fff9add5cf9c1705f1206
Kiirynn <kieran.poropat@gmail.com>
2022-11-27 08:03:58 -0500

    for nows

--- java.js
+++ java.js
@@ -372,7 +372,7 @@ const navSlide = () => {
              link.style.animation = '';
              }
              else {
-                 link.style.animation =  `navLinkFade 0.5s ease forwards ${index / 7 + .3}s`;
+                 link.style.animation =  `navLinkFade .3s ease forwards ${index / 7 + 0.5}s`;
              }
          });
 
